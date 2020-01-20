66edc2638bb79d4c931eb435e4e733b709d4efd9
Nikhil Devshatwar <nikhil.nd@ti.com>
2020-01-21 00:21:15 +0530

    soc: j721e: Partition nonsecure proxy and ring monitors
    Define the resources for non secure proxy and ring monitor
    allocation across different hosts.
    Update the total count of resources.
    
    Signed-off-by: Nikhil Devshatwar <nikhil.nd@ti.com>

--- soc/j721e/evm/sysfw_img_cfg.h
+++ soc/j721e/evm/sysfw_img_cfg.h
@@ -1,7 +1,7 @@
 /*
  * K3 System Firmware Configuration Data
  *
- * Copyright (C) 2019 Texas Instruments Incorporated - http://www.ti.com/
+ * Copyright (C) 2019-2020 Texas Instruments Incorporated - http://www.ti.com/
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -35,6 +35,6 @@
 #ifndef SYSFW_IMG_CFG_H
 #define SYSFW_IMG_CFG_H
 
-#define BOARDCFG_RM_RESASG_ENTRIES	258
+#define BOARDCFG_RM_RESASG_ENTRIES	305
 
 #endif /* SYSFW_IMG_CFG_H */
