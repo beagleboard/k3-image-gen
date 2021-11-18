b0e094ba91bf551fc4513f2302993234b99c1c16
Yogesh Siraswar <yogeshs@ti.com>
2021-11-18 17:21:21 +0000

    sysfw_boardcfg_validator: Add j721e_sr1_1 device support
    Add j721e_sr1_1 device support
    
    Change suggested by Vignesh:
    scripts/sysfw_boardcfg_rules.json does not have rules to validate
    j721e_sr1_1. Alias j721e_sr1_1 to j721e for now:
    
    Signed-off-by: Vignesh Raghavendra <vigneshr@ti.com>
    Signed-off-by: Yogesh Siraswar <yogeshs@ti.com>

--- scripts/sysfw_boardcfg_validator.py
+++ scripts/sysfw_boardcfg_validator.py
@@ -539,7 +539,7 @@ class sysfw_trace_cli:
             help="SoC supported by input binary",
             action="store",
             type=str,
-            choices={'am65x', 'am65x_sr2', 'am64x', 'j721e', 'j721e_legacy', 'j7200'},
+            choices={'am65x', 'am65x_sr2', 'am64x', 'j721e', 'j721e_legacy', 'j721e_sr1_1', 'j7200'},
             required=True)
 
         # Required output arguments
@@ -603,6 +603,9 @@ class sysfw_trace_cli:
 
         if self.cmd_args.soc == 'am65x':
             self.cmd_args.soc = 'am6'
+
+        if self.cmd_args.soc == 'j721e_sr1_1':
+            self.cmd_args.soc = 'j721e'
 
     def process_data(self):
         self.rules.process_data(
