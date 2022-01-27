f9beeada4481a1f2fe7f6b54d2ccade50f72558b
Aswath Govindraju <a-govindraju@ti.com>
2022-01-27 16:58:10 +0530

    scripts: sysfw_boardcfg_*: Remove j721e_legacy
    Remove j721e_legacy configuration.
    
    Signed-off-by: Aswath Govindraju <a-govindraju@ti.com>
    Signed-off-by: Vignesh Raghavendra <vigneshr@ti.com>

--- scripts/sysfw_boardcfg_validator.py
+++ scripts/sysfw_boardcfg_validator.py
@@ -539,7 +539,7 @@ class sysfw_trace_cli:
             help="SoC supported by input binary",
             action="store",
             type=str,
-            choices={'am65x', 'am65x_sr2', 'am64x', 'j721e', 'j721e_legacy', 'j721e_sr1_1', 'j7200'},
+            choices={'am65x', 'am65x_sr2', 'am64x', 'j721e', 'j721e_sr1_1', 'j7200'},
             required=True)
 
         # Required output arguments
