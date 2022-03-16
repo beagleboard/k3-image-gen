dc275ed3f2530f3ca646851401f0631a38f8c7d9
Vignesh Raghavendra <vigneshr@ti.com>
2022-03-16 11:20:06 +0530

    scripts: sysfw_boarcfg_*: Add AM62x specific data
    In preparation for adding AM62x support, update validator scripts and
    boardcfg rules with AM62x specific rules.
    
    Signed-off-by: Vignesh Raghavendra <vigneshr@ti.com>

--- scripts/sysfw_boardcfg_validator.py
+++ scripts/sysfw_boardcfg_validator.py
@@ -539,7 +539,7 @@ class sysfw_trace_cli:
             help="SoC supported by input binary",
             action="store",
             type=str,
-            choices={'am65x', 'am65x_sr2', 'am64x', 'j721e', 'j721e_sr1_1', 'j7200', 'j721s2'},
+            choices={'am65x', 'am65x_sr2', 'am64x', 'j721e', 'j721e_sr1_1', 'j7200', 'j721s2', 'am62x'},
             required=True)
 
         # Required output arguments
